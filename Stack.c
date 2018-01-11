df56f3855b6ff1d30a421c6ef51b7d3ff035a87a
yashchks87 <yashchks87@gmail.com>
2018-01-11 15:04:33 +0530

    Changes to be committed:

---
+++ Stack.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+// int MAX=10;
+#define MAX 10
+int st[MAX];
+int top= -1;
+int push();
+int pop();
+int peep();
+int display();
+int main()
+{
+  int option;
+  while(option!=0)
+  {
+    printf("******MAIN MENU******\n");
+    printf("Folks are you ready to some play with stack?????\n");
+    printf("1.PUSH\n");
+    printf("2.POP\n");
+    printf("3.PEEP\n");
+    printf("4.DISPLAY\n");
+    printf("Choose appropriate option\n");
+    scanf("%d",&option);
+    switch(option)
+    {
+      case 1:
+      {
+        // It will add value in stack.
+        push();
+        break;
+      }
+      case 2:
+      {
+        // It will remove value from stack.
+        pop();
+        break;
+      }
+      case 3:
+      {
+        // It will display top-most value in stack.
+        peep();
+        break;
+      }
+      case 4:
+      {
+        // It will display whole stack.
+        display();
+        break;
+      }
+    }
+  }
+  return 0;
+}
+int push()
+{
+  int val;
+  printf("Enter value to push in stack.\n");
+  scanf("%d\n",&val);
+  if(top == MAX-1)
+  {
+    printf("everything is full, let me empty first...\n");
+  }
+  else
+  {
+    top+=1;
+    st[top]=val;
+  }
+  return 0;
+}
+int pop()
+{
+  int del_value;
+  if(top == -1)
+  {
+    printf("Nothing to delete.");
+  }
+  else
+  {
+    del_value = st[top];
+    top--;
+  }
+  return 0;
+}
+int display()
+{
+  // int i;
+  if(top==-1)
+  {
+    printf("Nothing to show, stack is empty.\n");
+  }
+  else
+  {
+    for(int i=top;i>=0;i--)
+    {
+      printf("%d\n",st[i]);
+    }
+  }
+  return 0;
+}
+int peep()
+{
+  int top_value;
+  if(top==-1)
+  {
+    printf("Nothing to print....\n");
+  }
+  else
+  {
+    top_value=st[top];
+    printf("The top most value is %d\n",top_value);
+  }
+  return 0;
+}
